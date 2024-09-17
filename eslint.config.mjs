2e321e4659ade876a13aa07ef038bfe0b1089ced
brunomacedo-coxbr <bruno.macedo@coxautomotive.com.br>
2024-09-17 19:23:46 -0300

    feat(eslint): setup airbnb base rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,15 @@ import pluginJs from "@eslint/js";
 import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
 import parser from "@typescript-eslint/parser";
 import playwright from "eslint-plugin-playwright";
+import importPlugin from "eslint-plugin-import";
+
+import style from "eslint-config-airbnb-base/rules/style";
+import bestPractices from "eslint-config-airbnb-base/rules/best-practices";
+import variables from "eslint-config-airbnb-base/rules/variables";
 
 export default [
   {
-    files: ["tests/**/*.{js,mjs,cjs,ts}"],
+    files: ["tests/**/*.{js,ts}"],
     languageOptions: {
       globals: globals.browser,
       parser: parser,
@@ -14,13 +19,15 @@ export default [
     plugins: {
       "@typescript-eslint": tsEslintPlugin,
       "playwright": playwright,
+      "import": importPlugin,
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
       ...tsEslintPlugin.configs.recommended.rules,
       ...playwright.configs.recommended.rules,
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
+      ...style.rules,
+      ...bestPractices.rules,
+      ...variables.rules,
     },
   },
 ];
